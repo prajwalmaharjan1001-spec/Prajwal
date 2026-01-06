904c0418a8366476e04214b97c38720a6c8ba9fc
Prajwal Maharjan <prajwalmaharjan1001@gmail.com>
2026-01-06 17:04:34 +0545

    js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const person = 'Prajwal';
+//const person = 'Prajwal';
 // const personName = 'He\'s 20 years old\nHi';
 // const person_name = 'hello'
 
@@ -31,9 +31,71 @@ const person = 'Prajwal';
 
 // console.log(personName.split('.'));
     
-const personName = 'Ram';
-const age = 90;
-const job  ='Dev';
-// template literals
-console.log(`he is ${personName} ${age} ${job} `);
-hejhdxxjjjjjjjjj
+// const personName = 'Ram';
+// const age = 90;
+// const job  ='Dev';
+// // template literals
+// console.log(`he is ${personName} ${age} ${job} `);
+
+
+// const strongPasswordRegex =
+//   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+// console.log(strongPasswordRegex.test('Abscbw@9'));
+
+// const temperature = 20;
+// const value = 2;
+// const pizza = 'pizza';
+// const pasta = 'pasta';
+
+// Write a program that produces this output by using all of them:
+
+// The temperature is 20C
+// 2 plus 2 makes 4
+// I like pizza and pasta
+
+// console.log(`The temperature is ${temperature}C\n${value} plus ${value} makes ${value+value}\nI like ${pizza} and ${pasta}.`);
+
+// let m = 'sdlkf';
+
+// m = 'sdflk';
+// console.log(m);
+
+// const price = '10000';
+// console.log(Number(price)+1000);
+
+
+//const price = 111;
+//console.log(Number.isInteger(price));
+
+
+//const price = 'abc';
+// const price = '555';
+
+// console.log(Number.isNaN(Number(price)));
+
+// const person={
+//     name: 'prajwal',
+//     age: 20,
+//     sleep:function(){
+//         console.log('person is sleeping')
+//     }
+// }
+
+// const carName = {
+//     name: 'Supra',
+//     model:'Electric',
+//     speed: function(){
+//         console.log('It\'s high spped is 160km/hrs')
+//     }
+// };
+// carName.speed();
+// console.log(carName.model);
+
+console.log(Math.PI);
+console.log(Math.sqrt(4));
+console.log(Math.cbrt(27));
+console.log(Math.max(11, 99, 77, 66, 4));
+console.log(`The max value is ${Math.max(11, 99, 77, 66, 4)}`);
+console.log(Math.min(11, 99, 77, 1, 66, 4, 0.5));
