8c6fe6caba05aed20b5fc819f2903b2a650c071f
Prajwal Maharjan <prajwalmaharjan1001@gmail.com>
2026-01-06 15:58:48 +0545

    js

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const person = 'Prajwal';
-const personName = 'He\'s 20 years old';
-const person_name = 'hello'
+// const personName = 'He\'s 20 years old\nHi';
+// const person_name = 'hello'
 
-console.log(person);
+// console.log(person.at(-1));
+// console.log(person.charAt(1))//(-)dosent support
+// console.log(person.concat(' \nHello').concat('\nsello')) //value return garxa
+// const personName = 'hello';
+
+// const name = 'shyam';
+// const job = 'Dev';
+
+// console.log(name.concat('-').concat(job).concat('-').concat('Jee'));
+// shyam-Dev-jee
+// console.log(person.toUpperCase())
+// console.log(person.toLowerCase())
+// console.log(personName.endsWith('a'));
+//console.log(personName.includes('ram'));
+// console.log(personName.isWellFormed(''));
+// console.log(personName.length);
+// console.log(personName.trim().length);
+// console.log(personName.repeat(5));
+//console.log(personName.replace('bad','good'));
+//console.log(personName.replaceAll('bad','good'));
+//console.log(personName.slice(0,4));
+// console.log(personName.substring(0,2));
+    
+// const personName = 'zcnzcnmskdnk.jpg';
+
+
+
+// console.log(personName.split('.'));
+    
+const personName = 'Ram';
+const age = 90;
+const job  ='Dev';
+// template literals
+console.log(`he is ${personName} ${age} ${job} `);
